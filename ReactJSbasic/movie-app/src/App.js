ab14006c214003ee1f083ed67ab6ad8db0ddc9ca
Yona <118460222+yosoyyona@users.noreply.github.com>
2024-02-14 22:30:22 +0100

    components, routes

--- ReactJSbasic/movie-app/src/App.js
+++ ReactJSbasic/movie-app/src/App.js
@@ -1,50 +1,23 @@
-import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  // async function
-  const getMovies = async() => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json(); 
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  useEffect(() => {
-    getMovies();
-  }, [])
-
-  // using just fetch
-  useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`)
-    .then((response => response.json()))
-    .then((json) => {
-      setMovies(json.data.movies);
-      setLoading(false);
-    });
-  }, [])
-  
   return (
-    <div>
-      {loading ? <h1>Loading...</h1> : 
-        <div>
-          {movies.map((movie) => 
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
-            <p>Summary: {movie.summary}</p>
-            <ul>
-              {movie.genres.map((g) => <li key={g}>{g}</li>)}
-            </ul>
-          </div>
-          )}
-        </div>
-      }
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie"> {/* upper */}
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
